4d64113f0a11ed14bb7d5c411ca3997b63caee70
Timo Schöpflin <timo@schoepflin.io>
2022-11-06 07:11:20 +0100

    Store incomming request in blob storage

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -1,13 +1,18 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    context.log(req.method);
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+module.exports = async function (context, req) {
+    
+    const body = req.body;
+    context.uuid = req.query.uuid;
+    context.game = req.query.game
+    context.user = req.query.user
+
+    if ('POST' != req.method) {
+        context.res = {status: 405, body: `Method '${req.method}' not allowed!` };
+    } else if(context.uuid && context.game && context.user && body) {
+        context.bindings.outBlob = req.body;
+        context.res = { status: 201 };
+    } else {
+        context.res = {status: 400, body: 'Bad Request!' };
+    }
+    
 }

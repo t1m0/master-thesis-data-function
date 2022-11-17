2e985750f7f71754076686a1d4cbd24ce1bbc954
Timo Schöpflin <timo@schoepflin.io>
2022-11-17 09:14:58 +0100

    Drop later

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -1,18 +1,21 @@
 
 module.exports = async function (context, req) {
-    
-    const body = req.body;
-    context.uuid = req.query.uuid;
-    context.game = req.query.game
-    context.user = req.query.user
+    try {
+        const body = req.body;
+        context.uuid = req.query.uuid;
+        context.game = req.query.game
+        context.user = req.query.user
 
-    if ('POST' != req.method || 'PUT' != req.method) {
-        context.res = {status: 405, body: `Method '${req.method}' not allowed!` };
-    } else if(context.uuid && context.game && context.user && body) {
-        context.bindings.outBlob = req.body;
-        context.res = { status: 201 }; 
-    } else {
-        context.res = {status: 400, body: 'Bad Request!' };
+        if ('POST' != req.method || 'PUT' != req.method) {
+            context.res = {status: 405, body: `Method '${req.method}' not allowed!` };
+        } else if(context.uuid && context.game && context.user && body) {
+            context.bindings.outBlob = req.body;
+            context.res = { status: 201 }; 
+        } else {
+            context.res = {status: 400, body: 'Bad Request!' };
+        }
+    } catch (err) {
+        context.res = {status: 400, body: err };
     }
     
 }
